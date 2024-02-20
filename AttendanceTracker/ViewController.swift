f5c63aee3cab8caeda31e1dc2443dffa04673b4a
stassie-lzrv <adlazareva@edu.hse.ru>
2024-02-21 00:12:52 +0300

    Initial Commit

---
+++ AttendanceTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AttendanceTracker
+//
+//  Created by Настя Лазарева on 21.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
